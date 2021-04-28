6fe3487b665d769a8ff379c66ab07c16b522873b
jsevo <jonas.schluter@nyulangone.org>
2021-04-27 22:02:09 -0400

    blacken formatting

--- mtist/lvsimulator.py
+++ mtist/lvsimulator.py
@@ -1,7 +1,7 @@
 # Author: Jonas Schluter <jonas.schluter+github@gmail.com>
 #         http://www.ramenlabs.science
 #
-# License: MIT 
+# License: MIT
 import matplotlib
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
@@ -42,10 +42,11 @@ class LV(object):
     """
 
     def __init__(
-            self,
-            ecosystem=util.ecosystem,
-            growth_rates=util.growth_rates,
-            species_styles=util.species_styles, ):
+        self,
+        ecosystem=util.ecosystem,
+        growth_rates=util.growth_rates,
+        species_styles=util.species_styles,
+    ):
         # TODO: Refactor all code to make 'ecosystem' private and use get / set.
         self.ecosystem = ecosystem
         self.growth_rates = growth_rates
@@ -57,10 +58,7 @@ class LV(object):
     #     print("Setting up simulator for Lotka Volterra dynamics with ecosystem: \n")
     #     print(self.ecosystem)
 
-    def add_n_random_species(self,
-                             n,
-                             random_seed=0,
-                             interaction_threshold=None):
+    def add_n_random_species(self, n, random_seed=0, interaction_threshold=None):
         """
         Add species to the ecosystem. Newyly added species will interact rarely and
         relatively weakly with other species, and will have -1 for interaction with self.
@@ -75,23 +73,22 @@ class LV(object):
         species_style = self.species_style
         np.random.seed(random_seed)
         all_interactions = util.construct_ecosystem(ecosystem.keys(), ecosystem)
-        mean_interactions_magnitude = np.mean(
-            np.mean(np.abs(all_interactions)))
+        mean_interactions_magnitude = np.mean(np.mean(np.abs(all_interactions)))
         if not interaction_threshold is None:
             iat = interaction_threshold
         else:
             iat = 0.7
         iA = lambda x: x if np.abs(x) < iat * mean_interactions_magnitude else 0
         for i in range(0, n):
-            ecosystem['zz_random_sp' + str(i)] = {}
-            growth_rates['zz_random_sp' + str(i)] = np.random.randn() * 0.25
+            ecosystem["zz_random_sp" + str(i)] = {}
+            growth_rates["zz_random_sp" + str(i)] = np.random.randn() * 0.25
         curr_species = ecosystem.keys()
         for i in range(0, n):
             for k in list(curr_species):
-                ecosystem['zz_random_sp' + str(i)][k] = iA(np.random.randn())
-                ecosystem[k]['zz_random_sp' + str(i)] = iA(np.random.randn())
-            ecosystem['zz_random_sp' + str(i)]['zz_random_sp' + str(i)] = -1
-        species_style['zz_random_sp'] = {'c': '#03719c', 's': '-', 'w': .75}
+                ecosystem["zz_random_sp" + str(i)][k] = iA(np.random.randn())
+                ecosystem[k]["zz_random_sp" + str(i)] = iA(np.random.randn())
+            ecosystem["zz_random_sp" + str(i)]["zz_random_sp" + str(i)] = -1
+        species_style["zz_random_sp"] = {"c": "#03719c", "s": "-", "w": 0.75}
         self.ecosystem = ecosystem
         self.growth_rates = growth_rates
         self.species_style = species_style
@@ -107,9 +104,10 @@ class LV(object):
     def remove_random_species(self):
         """removes all the 'random' species which interacted weakly with the core ecosystem."""
         core_eco = self.ecosystem
-        drop_rand = lambda d : dict([(k, d[k]) for k in d.keys() if 'random' not in k])
-        self.ecosystem = dict([(k, drop_rand(core_eco[k])) for k in core_eco.keys()
-                          if 'zz' not in k])
+        drop_rand = lambda d: dict([(k, d[k]) for k in d.keys() if "random" not in k])
+        self.ecosystem = dict(
+            [(k, drop_rand(core_eco[k])) for k in core_eco.keys() if "zz" not in k]
+        )
         self.growth_rates = drop_rand(self.growth_rates)
         self.species_style = drop_rand(self.species_style)
         self.species_names = list(sorted(self.ecosystem.keys()))
@@ -121,19 +119,17 @@ class LV(object):
         A_full = pd.DataFrame(
             np.zeros((n_species, n_species + 1)),
             index=self.species_names,
-            columns=['mu'] + self.species_names)
+            columns=["mu"] + self.species_names,
+        )
         for i, focalspecies in enumerate(self.species_names):
             A_full.loc[focalspecies][:] = np.append(
-                self.growth_rates[focalspecies], self.A[i])
-        return (A_full)
+                self.growth_rates[focalspecies], self.A[i]
+            )
+        return A_full
 
-    def run_lv(self,
-               random_seed=0,
-               tend=5,
-               dt=.1,
-               yinit_specific={},
-               noise=0,
-               sample_freq=50):
+    def run_lv(
+        self, random_seed=0, tend=5, dt=0.1, yinit_specific={}, noise=0, sample_freq=50
+    ):
         """Simulate ecosystem dynamics over time using the VODE solver.
 
         Parameters
@@ -165,7 +161,8 @@ class LV(object):
             dt=dt,
             yinit_specific=yinit_specific,
             noise=noise,
-            sample_freq=sample_freq)
+            sample_freq=sample_freq,
+        )
         return (sol_t, sol_y, sol_t_all, sol_y_all, first_yinit)
 
     def plot_lv(self, t=None, y=None, yinit=[], savepath=None, figure=None, axes=None):
@@ -181,12 +178,13 @@ class LV(object):
         fig, ax1, ax2 : matplotlib.figure, ax handles
 
         """
-        if len(yinit)==0:
+        if len(yinit) == 0:
             t, y, sol_t_all, sol_y_all, yinit = self.run_lv()
-        fig, ax1, ax2 = util.plot_lv(t, y, self.ecosystem, self.species_style,
-                                     yinit, figure=figure, axes=axes)
+        fig, ax1, ax2 = util.plot_lv(
+            t, y, self.ecosystem, self.species_style, yinit, figure=figure, axes=axes
+        )
         if savepath:
-            fig.savefig(savepath, bbox_inches='tight', dpi=300)
+            fig.savefig(savepath, bbox_inches="tight", dpi=300)
         return (fig, ax1, ax2)
 
     def simple_plot_lv(self, t=None, y=None, yinit=[], savepath=None):
@@ -202,20 +200,21 @@ class LV(object):
         fig, ax1, ax2 : matplotlib.figure, ax handles
 
         """
-        if len(yinit)==0:
+        if len(yinit) == 0:
             t, y, sol_t_all, sol_y_all, yinit = self.run_lv()
-        fig, ax1 = util.simple_plot_lv(t, y, self.ecosystem, self.species_style,
-                                     yinit)
+        fig, ax1 = util.simple_plot_lv(t, y, self.ecosystem, self.species_style, yinit)
         if savepath:
-            fig.savefig(savepath, bbox_inches='tight', dpi=300)
+            fig.savefig(savepath, bbox_inches="tight", dpi=300)
         return (fig, ax1)
 
-    def plot_ecosystem(self,
-                       species_to_plot=None,
-                       savepath=None,
-                       rename_species=False,
-                       annotate=False,
-                       format='png'):
+    def plot_ecosystem(
+        self,
+        species_to_plot=None,
+        savepath=None,
+        rename_species=False,
+        annotate=False,
+        format="png",
+    ):
         """Plot a simple heatmap showing between-species interactions.
         """
         A_true = self.get_ecosystem_as_df()
@@ -223,38 +222,43 @@ class LV(object):
             if type(rename_species) == dict:
                 rename_dict = rename_specie
             else:
-                rename_dict = dict([(z, 'rs' + z[-4::])
-                                    if 'zz' in z else ('mu', '$\mu$')
-                                    for z in A_true.columns])
+                rename_dict = dict(
+                    [
+                        (z, "rs" + z[-4::]) if "zz" in z else ("mu", "$\mu$")
+                        for z in A_true.columns
+                    ]
+                )
                 A_true = A_true.rename(rename_dict, columns=rename_dict)
         if species_to_plot:
-            A_true = A_true[['mu'] + species_to_plot]
+            A_true = A_true[["mu"] + species_to_plot]
         fig, ax = plt.subplots()
         gs = gridspec.GridSpec(1, 15)
         ## Time series
         axl = plt.subplot(gs[0, 14])
         axp = plt.subplot(gs[0, 0:14])
         sns.heatmap(
-            A_true.rename(columns={'mu': '$\mu$'}),
+            A_true.rename(columns={"mu": "$\mu$"}),
             vmax=3,
             vmin=-6,
-            annot = annotate,
+            annot=annotate,
             annot_kws={"size": 10},
             ax=axp,
             cbar_ax=axl,
-            cbar_kws={"orientation": "vertical"})
+            cbar_kws={"orientation": "vertical"},
+        )
         plt.xticks(rotation=90)
         plt.yticks(rotation=0)
         fig.set_size_inches(5, 5)
-        axp.set_title('Interaction matrix', fontdict=util.font_axes)
+        axp.set_title("Interaction matrix", fontdict=util.font_axes)
         axl.set_yticks([-6, -5, -2, 0, 2, 4, 6])
         axl.set_yticklabels(
-            ['-15', '-8', '-2', '0', '2', '4', '6'], fontdict=util.font_ticks)
+            ["-15", "-8", "-2", "0", "2", "4", "6"], fontdict=util.font_ticks
+        )
         # cbar = axl.collections[0].colorbar
         # cbar.set_ticks([-6,-5, 0,  3])
         # cbar.set_ticklabels([-15,-5,  0,  3])
         if savepath:
-            if ( format=='eps' ) | ( format=='pdf' ):
-                matplotlib.rcParams['pdf.fonttype'] = 42
-                matplotlib.rcParams['ps.fonttype'] = 42
-            fig.savefig(savepath, bbox_inches='tight', format=format, dpi=300)
+            if (format == "eps") | (format == "pdf"):
+                matplotlib.rcParams["pdf.fonttype"] = 42
+                matplotlib.rcParams["ps.fonttype"] = 42
+            fig.savefig(savepath, bbox_inches="tight", format=format, dpi=300)
--- mtist/lvutils.py
+++ mtist/lvutils.py
@@ -1,7 +1,7 @@
 # Author: Jonas Schluter <jonas.schluter+github@gmail.com>
-#        www.ramenlabs.science 
+#        www.ramenlabs.science
 #
-# License: MIT 
+# License: MIT
 
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
@@ -10,252 +10,216 @@ import pandas as pd
 import scipy.integrate as sp
 import seaborn as sns
 import warnings
+
 # PNAS figure guidelines as ref for sizes
 #
-#1 column wide (20.5 picas / 3.42” / 8.7cm)
-#1.5 columns wide (27 picas / 4.5” / 11.4cm)
-#2 columns wide (42.125 picas / 7” / 17.8cm)
+# 1 column wide (20.5 picas / 3.42” / 8.7cm)
+# 1.5 columns wide (27 picas / 4.5” / 11.4cm)
+# 2 columns wide (42.125 picas / 7” / 17.8cm)
 
 small_square_fig = (3.42, 3.42)
 small_wide_fig = (3.42, 2.5)
 small_long_fig = (3.42, 8)
 large_wide_fig = (7, 3.5)
 font_axes = {
-    'family': 'Arial',
-    'color': 'black',
-    'weight': 'normal',
-    'size': 12,
+    "family": "Arial",
+    "color": "black",
+    "weight": "normal",
+    "size": 12,
 }
 
 font_ticks = {
-    'family': 'Arial',
-    'color': 'black',
-    'weight': 'normal',
-    'size': 8,
+    "family": "Arial",
+    "color": "black",
+    "weight": "normal",
+    "size": 8,
 }
 ecosystem = {
-    'Antilope': {
-        'Antilope': -1,
-        'Baboon': 0,
-        'Bison': -2,
-        'Buffalo': -2,
-        'Cheetah': -.1,
-        'Duck': 0,
-        'Eagle': 1,
-        'Lion': -.2,
-        'Tiger': -.2,
-        'Zebra': -2
+    "Antilope": {
+        "Antilope": -1,
+        "Baboon": 0,
+        "Bison": -2,
+        "Buffalo": -2,
+        "Cheetah": -0.1,
+        "Duck": 0,
+        "Eagle": 1,
+        "Lion": -0.2,
+        "Tiger": -0.2,
+        "Zebra": -2,
     },
-    'Baboon': {
-        'Antilope': 0,
-        'Baboon': -1,
-        'Bison': 0,
-        'Buffalo': 0,
-        'Cheetah': -.5,
-        'Duck': 0,
-        'Eagle': -1,
-        'Lion': -.5,
-        'Tiger': -.5,
-        'Zebra': 0
+    "Baboon": {
+        "Antilope": 0,
+        "Baboon": -1,
+        "Bison": 0,
+        "Buffalo": 0,
+        "Cheetah": -0.5,
+        "Duck": 0,
+        "Eagle": -1,
+        "Lion": -0.5,
+        "Tiger": -0.5,
+        "Zebra": 0,
     },
-    'Bison': {
-        'Antilope': -.5,
-        'Baboon': 0,
-        'Bison': -.5,
-        'Buffalo': -2,
-        'Cheetah': -.5,
-        'Duck': 0,
-        'Eagle': 1,
-        'Lion': -1,
-        'Tiger': -1,
-        'Zebra': -2
+    "Bison": {
+        "Antilope": -0.5,
+        "Baboon": 0,
+        "Bison": -0.5,
+        "Buffalo": -2,
+        "Cheetah": -0.5,
+        "Duck": 0,
+        "Eagle": 1,
+        "Lion": -1,
+        "Tiger": -1,
+        "Zebra": -2,
     },
-    'Buffalo': {
-        'Antilope': -.5,
-        'Baboon': 0,
-        'Bison': -2,
-        'Buffalo': -.5,
-        'Cheetah': -.5,
-        'Duck': 0,
-        'Eagle': 1,
-        'Lion': -1,
-        'Tiger': -1,
-        'Zebra': -2
+    "Buffalo": {
+        "Antilope": -0.5,
+        "Baboon": 0,
+        "Bison": -2,
+        "Buffalo": -0.5,
+        "Cheetah": -0.5,
+        "Duck": 0,
+        "Eagle": 1,
+        "Lion": -1,
+        "Tiger": -1,
+        "Zebra": -2,
     },
-    'Cheetah': {
-        'Antilope': .5,
-        'Baboon': .125,
-        'Bison': 1.25,
-        'Buffalo': 1.5,
-        'Cheetah': -2.5,
-        'Duck': .15,
-        'Eagle': 0,
-        'Lion': 0,
-        'Tiger': 0,
-        'Zebra': 1.5
+    "Cheetah": {
+        "Antilope": 0.5,
+        "Baboon": 0.125,
+        "Bison": 1.25,
+        "Buffalo": 1.5,
+        "Cheetah": -2.5,
+        "Duck": 0.15,
+        "Eagle": 0,
+        "Lion": 0,
+        "Tiger": 0,
+        "Zebra": 1.5,
     },
-    'Duck': {
-        'Antilope': -.01,
-        'Baboon': 0,
-        'Bison': -.02,
-        'Buffalo': -.02,
-        'Cheetah': -.01,
-        'Duck': -5,
-        'Eagle': 0,
-        'Lion': -.15,
-        'Tiger': -.15,
-        'Zebra': -.02
+    "Duck": {
+        "Antilope": -0.01,
+        "Baboon": 0,
+        "Bison": -0.02,
+        "Buffalo": -0.02,
+        "Cheetah": -0.01,
+        "Duck": -5,
+        "Eagle": 0,
+        "Lion": -0.15,
+        "Tiger": -0.15,
+        "Zebra": -0.02,
     },
-    'Eagle': {
-        'Antilope': 0.01,
-        'Baboon': .5,
-        'Bison': 0,
-        'Buffalo': 0,
-        'Cheetah': 0,
-        'Duck': 0,
-        'Eagle': -1,
-        'Lion': 0,
-        'Tiger': 0,
-        'Zebra': 0
+    "Eagle": {
+        "Antilope": 0.01,
+        "Baboon": 0.5,
+        "Bison": 0,
+        "Buffalo": 0,
+        "Cheetah": 0,
+        "Duck": 0,
+        "Eagle": -1,
+        "Lion": 0,
+        "Tiger": 0,
+        "Zebra": 0,
     },
-    'Lion': {
-        'Antilope': 1,
-        'Baboon': .25,
-        'Bison': 2.5,
-        'Buffalo': 3,
-        'Cheetah': 0,
-        'Duck': .15,
-        'Eagle': 0,
-        'Lion': -1,
-        'Tiger': -15,
-        'Zebra': 3
+    "Lion": {
+        "Antilope": 1,
+        "Baboon": 0.25,
+        "Bison": 2.5,
+        "Buffalo": 3,
+        "Cheetah": 0,
+        "Duck": 0.15,
+        "Eagle": 0,
+        "Lion": -1,
+        "Tiger": -15,
+        "Zebra": 3,
     },
-    'Tiger': {
-        'Antilope': 1,
-        'Baboon': .25,
-        'Bison': 2.5,
-        'Buffalo': 3,
-        'Cheetah': 0,
-        'Duck': .15,
-        'Eagle': 0,
-        'Lion': -15,
-        'Tiger': -1,
-        'Zebra': 3
+    "Tiger": {
+        "Antilope": 1,
+        "Baboon": 0.25,
+        "Bison": 2.5,
+        "Buffalo": 3,
+        "Cheetah": 0,
+        "Duck": 0.15,
+        "Eagle": 0,
+        "Lion": -15,
+        "Tiger": -1,
+        "Zebra": 3,
     },
-    'Zebra': {
-        'Antilope': -.25,
-        'Baboon': 0,
-        'Bison': -1,
-        'Buffalo': -1,
-        'Cheetah': -.5,
-        'Duck': -.1,
-        'Eagle': 0,
-        'Lion': -1,
-        'Tiger': -1,
-        'Zebra': -.5,
-    }
+    "Zebra": {
+        "Antilope": -0.25,
+        "Baboon": 0,
+        "Bison": -1,
+        "Buffalo": -1,
+        "Cheetah": -0.5,
+        "Duck": -0.1,
+        "Eagle": 0,
+        "Lion": -1,
+        "Tiger": -1,
+        "Zebra": -0.5,
+    },
 }
 growth_rates = {
-    'Antilope': .85,
-    'Baboon': 1,
-    'Bison': .6,
-    'Buffalo': .6,
-    'Cheetah': -.75,
-    'Duck': .3,
-    'Eagle': .8,
-    'Lion': -1.5,
-    'Tiger': -1.5,
-    'Zebra': .65
+    "Antilope": 0.85,
+    "Baboon": 1,
+    "Bison": 0.6,
+    "Buffalo": 0.6,
+    "Cheetah": -0.75,
+    "Duck": 0.3,
+    "Eagle": 0.8,
+    "Lion": -1.5,
+    "Tiger": -1.5,
+    "Zebra": 0.65,
 }
 species_styles = {
-    'Antilope': {
-        'c': '#ceb301',
-        's': '-',
-        'w': 1
-    },
-    'Baboon': {
-        'c': '#a00498',
-        's': '-',
-        'w': 1
-    },
-    'Bison': {
-        'c': '#88b378',
-        's': '-',
-        'w': 3
-    },
-    'Buffalo': {
-        'c': '#88b378',
-        's': '--',
-        'w': 3
-    },
-    'Cheetah': {
-        'c': '#d9544d',
-        's': ':',
-        'w': 2
-    },
-    'Duck': {
-        'c': '#a8a495',
-        's': '-',
-        'w': 1
-    },
-    'Eagle': {
-        'c': '#9dbcd4',
-        's': '-',
-        'w': 1
-    },
-    'Lion': {
-        'c': '#ef4026',
-        's': '-',
-        'w': 3
-    },
-    'Tiger': {
-        'c': '#ef4026',
-        's': '--',
-        'w': 3
-    },
-    'Zebra': {
-        'c': '#070d0d',
-        's': '--',
-        'w': 1
-    }
+    "Antilope": {"c": "#ceb301", "s": "-", "w": 1},
+    "Baboon": {"c": "#a00498", "s": "-", "w": 1},
+    "Bison": {"c": "#88b378", "s": "-", "w": 3},
+    "Buffalo": {"c": "#88b378", "s": "--", "w": 3},
+    "Cheetah": {"c": "#d9544d", "s": ":", "w": 2},
+    "Duck": {"c": "#a8a495", "s": "-", "w": 1},
+    "Eagle": {"c": "#9dbcd4", "s": "-", "w": 1},
+    "Lion": {"c": "#ef4026", "s": "-", "w": 3},
+    "Tiger": {"c": "#ef4026", "s": "--", "w": 3},
+    "Zebra": {"c": "#070d0d", "s": "--", "w": 1},
 }
 
 
 def _lv(**kwargs):
     """Helper function to set up ODE system"""
     params = {
-        '_A': np.array([[-1, 1, 1], [-1, -1, -1], [3, -3, -1]]),
-        '_mu': np.array([.1, 2, .04]),
-        '_noise_scale': 0
+        "_A": np.array([[-1, 1, 1], [-1, -1, -1], [3, -3, -1]]),
+        "_mu": np.array([0.1, 2, 0.04]),
+        "_noise_scale": 0,
     }
     params.update(kwargs)
 
     def lv_model_full_system(t, y):
-        A = params['_A']
-        mu = params['_mu']
+        A = params["_A"]
+        mu = params["_mu"]
         dy = np.multiply(np.dot(A, y) + mu, y)
-        return (dy)
+        return dy
 
-    return (lv_model_full_system)
+    return lv_model_full_system
 
 
-def run_lv(A,
-           mu,
-           species_names,
-           random_seed=0,
-           tend=5,
-           dt=.1,
-           yinit_specific={},
-           noise=0,
-           sample_freq=100):
+def run_lv(
+    A,
+    mu,
+    species_names,
+    random_seed=0,
+    tend=5,
+    dt=0.1,
+    yinit_specific={},
+    noise=0,
+    sample_freq=100,
+):
     """Solve ODE ecosystem."""
     if sample_freq > 100:
         warnings.warn(
             "Currently noise is added 100 times over the total simulation period. Choosing a sample frequency higher than that is not supported at this point.",
-            RuntimeWarning)
+            RuntimeWarning,
+        )
     np.random.seed(seed=random_seed)
-    #Assign random initial densities
-    #update if specific starting densities are chosen
+    # Assign random initial densities
+    # update if specific starting densities are chosen
     yinit = np.random.randint(1, 10, len(mu)) / 100
     yinit = dict(zip(sorted(species_names), yinit))
     yinit.update(yinit_specific)
@@ -266,15 +230,15 @@ def run_lv(A,
     sol_y_all = np.array([])
     sol_t = np.array([])
     sol_y = np.array([])
-    #f
-    r = sp.ode(_lv(_A=A, _mu=mu)).set_integrator('vode')
+    # f
+    r = sp.ode(_lv(_A=A, _mu=mu)).set_integrator("vode")
     r.set_initial_value(yinit, 0)
     sol_t = np.append(sol_t, 0)
     sol_y = np.append(sol_y, yinit)
     timepoints = np.linspace(0, tend, 100)
     for i, t in enumerate(timepoints[0:-1]):
         tend = timepoints[i + 1]
-        r = sp.ode(_lv(_A=A, _mu=mu)).set_integrator('vode')
+        r = sp.ode(_lv(_A=A, _mu=mu)).set_integrator("vode")
         r.set_initial_value(yinit, timepoints[i])
         while r.successful() and r.t < tend:
             sol_t_all = np.append(sol_t_all, r.t)
@@ -311,7 +275,7 @@ def construct_ecosystem(species, ecosystem):
     A : list of lists, interaction matrix
     """
     A = [[ecosystem[s][ss] for ss in sorted(species)] for s in sorted(species)]
-    return (A)
+    return A
 
 
 def plot_lv(t, y, ecosystem, species_styles, y_init, figure=None, axes=None):
@@ -332,8 +296,8 @@ def plot_lv(t, y, ecosystem, species_styles, y_init, figure=None, axes=None):
     fig, ax, ax: current figure, axes for two plots
 
     """
-    sns.set_style('white')
-    if ( figure==None ) | ( axes==None ):
+    sns.set_style("white")
+    if (figure == None) | (axes == None):
         fig = plt.figure()
         gs = gridspec.GridSpec(1, 4)
         ax = plt.subplot(gs[0, 1::])
@@ -345,25 +309,27 @@ def plot_lv(t, y, ecosystem, species_styles, y_init, figure=None, axes=None):
         [ax2, ax] = axes
     ## Time series
     for i, s in enumerate(sorted(ecosystem.keys())):
-        assign_species_name = lambda s: s if 'random' not in s else 'zz_random_sp'
+        assign_species_name = lambda s: s if "random" not in s else "zz_random_sp"
         s = assign_species_name(s)
-        if 'random' not in s:
+        if "random" not in s:
             ax.plot(
                 t,
                 y[:, i],
-                linestyle=species_styles[s]['s'],
-                linewidth=species_styles[s]['w'],
-                color=species_styles[s]['c'],
-                label=s)
+                linestyle=species_styles[s]["s"],
+                linewidth=species_styles[s]["w"],
+                color=species_styles[s]["c"],
+                label=s,
+            )
         else:
             ax.plot(
                 t,
                 y[:, i],
                 alpha=0.2,
-                linestyle=species_styles[s]['s'],
-                linewidth=species_styles[s]['w'],
-                color=species_styles[s]['c'],
-                label='random_species')
+                linestyle=species_styles[s]["s"],
+                linewidth=species_styles[s]["w"],
+                color=species_styles[s]["c"],
+                label="random_species",
+            )
 
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = dict(zip(labels, handles))
@@ -372,58 +338,59 @@ def plot_lv(t, y, ecosystem, species_styles, y_init, figure=None, axes=None):
         [k for k in list(sorted(by_label.keys()))],
         bbox_to_anchor=(1.05, 1),
         loc=2,
-        borderaxespad=0.)
-    ax.set_xlabel('Time', fontdict=font_axes)
+        borderaxespad=0.0,
+    )
+    ax.set_xlabel("Time", fontdict=font_axes)
     ax.set_xlim([0, max(t)])
-    ax.set_ylabel('Density', fontdict=font_axes)
+    ax.set_ylabel("Density", fontdict=font_axes)
     yticks = ax.get_yticks()
     ax.set_yticks([yticks[0], yticks[-1]])
-    ax.get_yaxis().set_ticks_position('right')
-    ax.spines['top'].set_visible(False)
+    ax.get_yaxis().set_ticks_position("right")
+    ax.spines["top"].set_visible(False)
     ## Ininitial Conditions
-    bar_species = [
-        s for s in list(sorted(ecosystem.keys())) if 'random' not in s
-    ]
+    bar_species = [s for s in list(sorted(ecosystem.keys())) if "random" not in s]
     ax2.bar(
         range(0, len(bar_species)),
-        y_init[0:len(bar_species)],
-        color=[species_styles[s]['c'] for s in sorted(bar_species)])
+        y_init[0 : len(bar_species)],
+        color=[species_styles[s]["c"] for s in sorted(bar_species)],
+    )
     ax2.set_xticks([x + 0.25 for x in range(0, len(bar_species))])
-    ax2.set_xticklabels(
-        sorted(bar_species), rotation='vertical', fontdict=font_ticks)
+    ax2.set_xticklabels(sorted(bar_species), rotation="vertical", fontdict=font_ticks)
     yticks = ax2.get_yticks()
-    ax2.set_yticks([ yticks[0], yticks[-1] ])
+    ax2.set_yticks([yticks[0], yticks[-1]])
     return (fig, ax, ax2)
 
 
 def simple_plot_lv(t, y, ecosystem, species_styles, y_init):
     """Plot time series data of ODE simulations."""
-    sns.set_style('white')
+    sns.set_style("white")
     fig, ax1 = plt.subplots()
     gs = gridspec.GridSpec(1, 3)
     ## Time series
     ax = plt.subplot(gs[0, 0::])
 
     for i, s in enumerate(sorted(ecosystem.keys())):
-        assign_species_name = lambda s: s if 'random' not in s else 'zz_random_sp'
+        assign_species_name = lambda s: s if "random" not in s else "zz_random_sp"
         s = assign_species_name(s)
-        if 'random' not in s:
+        if "random" not in s:
             ax.plot(
                 t,
                 y[:, i],
-                linestyle=species_styles[s]['s'],
-                linewidth=species_styles[s]['w'],
-                color=species_styles[s]['c'],
-                label=s)
+                linestyle=species_styles[s]["s"],
+                linewidth=species_styles[s]["w"],
+                color=species_styles[s]["c"],
+                label=s,
+            )
         else:
             ax.plot(
                 t,
                 y[:, i],
                 alpha=0.1,
-                linestyle=species_styles[s]['s'],
-                linewidth=species_styles[s]['w'],
-                color=species_styles[s]['c'],
-                label='random_species')
+                linestyle=species_styles[s]["s"],
+                linewidth=species_styles[s]["w"],
+                color=species_styles[s]["c"],
+                label="random_species",
+            )
 
     handles, labels = plt.gca().get_legend_handles_labels()
     by_label = dict(zip(labels, handles))
@@ -432,10 +399,11 @@ def simple_plot_lv(t, y, ecosystem, species_styles, y_init):
         [k for k in list(sorted(by_label.keys()))],
         bbox_to_anchor=(1.05, 1),
         loc=2,
-        borderaxespad=0.)
-    ax.set_xlabel('Time', fontdict=font_axes)
+        borderaxespad=0.0,
+    )
+    ax.set_xlabel("Time", fontdict=font_axes)
     ax.set_xlim([0, max(t)])
-    ax.set_ylabel('Density', fontdict=font_axes)
+    ax.set_ylabel("Density", fontdict=font_axes)
     ax.set_yticks([])
     ## Ininitial Conditions
     return (fig, ax)
